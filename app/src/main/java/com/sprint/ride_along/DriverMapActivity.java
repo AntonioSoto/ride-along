f7ff4db5b47929c4308e5a3ec1d62294c7463f45
Soto <ja.soto.puerto@gmail.com>
2017-11-23 06:52:47 -0600

    Mapa del driver implementado, falta funcionalidad. Perfil del driver creado.

--- app/src/main/java/com/sprint/ride_along/DriverMapActivity.java
+++ app/src/main/java/com/sprint/ride_along/DriverMapActivity.java
@@ -1,16 +1,15 @@
 package com.sprint.ride_along;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,8 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private Location currentLocation;
-    private Marker currentMarker;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,29 +34,11 @@ public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCa
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        MyLocationListener myLocationListener = new MyLocationListener();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                2000,
-                1,
-                myLocationListener
-        );
-
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        registerButton = (Button) findViewById(R.id.button_RegisterDriver);
+        registerButton.setVisibility(View.GONE);
     }
 
     @Override
@@ -71,37 +51,38 @@ public class DriverMapActivity extends AppCompatActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+
+        CameraUpdate meridaCam = CameraUpdateFactory.newLatLngZoom(new LatLng(20.9802, -89.621935), 12);
+        mMap.moveCamera(meridaCam);
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+
+                selectPoint(latLng);
+            }
+        });
+
+        Toast.makeText(
+                this,
+                "Selecciona tus puntos de encuentro en el mapa",
+                Toast.LENGTH_LONG
+        ).show();
     }
 
-    private class MyLocationListener implements LocationListener {
+    private void selectPoint(LatLng userPoint){
 
-        @Override
-        public void onLocationChanged(Location location) {
+        mMap.addMarker(new MarkerOptions()
+                    .position(userPoint)
+                    .title("")
+                    .snippet("")
+        ).setPosition(userPoint);
 
-            //String message = "Latitud: "+location.getLatitude()+" Longitud: "+location.getLongitude();
-            //Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
-            //textView.setText(message);
+        registerButton.setVisibility(View.VISIBLE);
+    }
 
-            currentLocation = location;
-            this.setupMarker();
-        }
+    public void registerDriver(View view){
 
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {}
-        @Override
-        public void onProviderEnabled(String provider) {}
-        @Override
-        public void onProviderDisabled(String provider) {}
 
-        private void setupMarker(){
-
-            if(currentMarker == null){
-                currentMarker = mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()))
-                        .title("")
-                        .snippet(""));
-            }
-            currentMarker.setPosition(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
-        }
     }
 }
